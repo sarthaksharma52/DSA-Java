12b207fbb26eec06258af00f9ed6a06f51c3e822
Sarthak Sharma <sarthak.dec04@gmail.com>
2024-10-11 23:45:38 +0530

    easy level array question

---
+++ Array/cells-with-odd-values-in-a-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for (int[] index : indices) {
+            row[index[0]]++;
+            col[index[1]]++;
+        }
+
+        int oddCount = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((row[i] + col[j]) % 2 != 0) {
+                    oddCount++;
+                }
+            }
+        }
+
+        return oddCount;
+    }
+}
