8e56d4f10878fe1768ebfce08c54a8ea501f69ea
Sarthak Sharma <sarthak.dec04@gmail.com>
2024-10-21 22:41:07 +0530

    medium level solved by gpt q50.

---
+++ math/powX-n.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        long N = n; 
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1.0;
+        double currentProduct = x;
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= currentProduct;
+            }
+            currentProduct *= currentProduct;
+            N /= 2;
+        }
+
+        return result;
+    }
+}
