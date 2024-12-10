1a83a86827b2f4bddbe7de9105a457371a559a71
Sarthak Sharma <sarthak.dec04@gmail.com>
2024-12-10 11:49:00 +0530

    modify Q3152 using optimised approach

--- Array/special_array_2.java
+++ Array/special_array_2.java
@@ -22,3 +22,55 @@ class Solution {
     }
 
 }
+
+
+//////////////////////run all test case except last one   //////////////////////////////////////
+
+
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        boolean[] result = new boolean[queries.length];
+        for(int i=0;i<queries.length;i++){
+            result[i] = check(nums,queries[i][0],queries[i][1]);
+        }
+        return result;
+    }
+    private boolean check(int[] nums, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if ((nums[i] % 2) == (nums[i + 1] % 2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+///////   best approach run all test cases   ////////////
+
+
+
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+      boolean[] ans = new boolean[queries.length];
+      int[] parityIds = new int[nums.length];
+      int id = 0;
+      parityIds[0] = id;
+  
+      for (int i = 1; i < nums.length; ++i) {
+        if (nums[i] % 2 == nums[i - 1] % 2)
+          ++id;
+        parityIds[i] = id;
+      }
+  
+      for (int i = 0; i < queries.length; ++i) {
+        final int from = queries[i][0];
+        final int to = queries[i][1];
+        ans[i] = parityIds[from] == parityIds[to];
+      }
+  
+      return ans;
+    }
+  }
+
+
