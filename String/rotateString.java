65eee840e573a6367385437679500a040e284b95
Sarthak Sharma <sarthak.dec04@gmail.com>
2024-12-27 00:02:35 +0530

    new approach

--- String/rotateString.java
+++ String/rotateString.java
@@ -8,3 +8,26 @@ public class RotateString {
         String concatenated = s + s;
         return concatenated.contains(goal);
     }
+
+
+
+// /////////////////////////////////////////////  //
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int len = s.length();
+        
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < len; i++) {
+            String rotated = s.substring(i) + s.substring(0, i);
+            if (goal.equals(rotated)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
