955fc97e8d88bda25a0990f3729edf642d443117
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-01-29 21:49:26 +0530

    1 easy level question

---
+++ math/squidBank.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int K = sc.nextInt();
+		
+		int ans = (N-K)*10000;
+		
+		System.out.println(ans);
+
+	}
+}
