3801930e26325011f704dcbc926110d7496518ad
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-03-04 09:13:22 +0530

    1 easy level string question Q2942

---
+++ String/findWordContainingCharacter.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            if(words[i].contains(Character.toString(x))){
+                arr.add(i);
+            }
+        }
+        // int[] result = arr.toArray();
+        // return result;
+
+        return arr;
+    }
+}
