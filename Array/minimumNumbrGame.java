1e671a5b38deadb52eac3c13f63f964484ea477b
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-03-10 23:44:00 +0530

    1 easy level array question Q2974

---
+++ Array/minimumNumbrGame.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int[] arr = new int[n];
+
+        int index = 0;
+        for (int i = 0; i < n; i += 2) {
+            arr[index++] = nums[i + 1]; 
+            arr[index++] = nums[i]; 
+        }
+        return arr;
+    }
+}
