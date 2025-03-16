fce82077a86b2dfbfd7fc46d0f50e6c9f9fb4ffb
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-03-16 22:34:51 +0530

    1 easy level question hashmap by gpt Q290

---
+++ hashmap/wordPattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public static boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) return false;
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (charToWord.containsKey(c)) {
+                if (!charToWord.get(c).equals(word)) return false;
+            } else {
+                if (wordToChar.containsKey(word)) return false;
+                charToWord.put(c, word);
+                wordToChar.put(word, c);
+            }
+        }
+        return true;
+    }
+
+}
