4f01bd0b9220c479f04d1e778a7e55263b5b2733
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-03-25 23:49:18 +0530

    1 array medium level question Q134

---
+++ Array/gasStation.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0, totalCost = 0, tank = 0, startIndex = 0;
+        
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            
+            tank += gas[i] - cost[i];
+            
+            if (tank < 0) { 
+                startIndex = i + 1;
+                tank = 0;
+            }
+        }
+
+        return totalGas >= totalCost ? startIndex : -1;
+    }
+}
