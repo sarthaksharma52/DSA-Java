b079928d2e9aa9914f44a6bccf9d0ec36723f55e
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-03-30 23:30:37 +0530

    1 medium level linkedlist question Q92

---
+++ LinkedList/reverseLinkedList2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+        ListNode next;
+        ListNode prevSubList = null;
+
+        for (int i = 0; i <= right - left; i++) {
+            next = curr.next;
+            curr.next = prevSubList;
+            prevSubList = curr;
+            curr = next;
+        }
+
+        prev.next.next = curr;
+        prev.next = prevSubList;
+
+        return dummy.next;
+    }
+}
