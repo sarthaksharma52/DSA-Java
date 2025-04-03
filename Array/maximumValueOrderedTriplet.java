3227994ee4b57559afe1434b661ca08fbdeb9c7a
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-04-03 10:07:06 +0530

    optamised code for question 2874.

--- Array/maximumValueOrderedTriplet.java
+++ Array/maximumValueOrderedTriplet.java
@@ -1,3 +1,5 @@
+// approach using O(n^3) complexity
+
 class Solution {
     public long maximumTripletValue(int[] nums) {
         long ans = Integer.MIN_VALUE;
@@ -12,5 +14,24 @@ class Solution {
             }
         }
         return ans < 0 ? 0 : ans;
+    }
+}
+
+
+// approach using O(n) complexity
+
+
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long ans = 0;
+        long dif = 0;
+        long max = 0;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            ans = Math.max(ans,dif*nums[i]);
+            dif = Math.max(dif,max-nums[i]);
+            max = Math.max(max,nums[i]);
+        }
+        return ans;
     }
 }
