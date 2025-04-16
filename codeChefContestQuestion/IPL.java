9e5043c6adc7fb2ebb6047ef0eebc2018080ef84
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-04-16 23:26:35 +0530

    1 easy level if else question

---
+++ codeChefContestQuestion/IPL.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		if(x>=7){
+		    System.out.println("THALA");
+		}
+		else{
+		    System.out.println("BOOM");
+		}
+
+	}
+}
