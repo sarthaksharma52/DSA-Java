fc97dbea3da74c2a004abeae3236d3c094ab714b
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-05-02 23:28:38 +0530

    1 easy level bst question by gpt Q637

---
+++ BinaryTree/averageTreeOfBinaryTree.java
@@ -0,0 +1,43 @@
+package BinaryTree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                sum += curr.val;
+
+                if (curr.left != null) queue.offer(curr.left);
+                if (curr.right != null) queue.offer(curr.right);
+            }
+
+            result.add(sum / size); 
+        }
+
+        return result;
+       }
+}
