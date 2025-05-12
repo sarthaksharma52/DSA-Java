f152292784e0418ab057a890bade32fae8bc0b1c
Sarthak Sharma <130988802+sarthaksharma52@users.noreply.github.com>
2025-05-12 23:44:23 +0530

    Update FindPivotIndex.java

--- Searching/FindPivotIndex.java
+++ Searching/FindPivotIndex.java
@@ -5,7 +5,7 @@ class Solution {
             totalSum += num;
         }
 
-        int leftSum = 0;
+        int leftSum = 0; 
         for (int i = 0; i < nums.length; i++) {
             int rightSum = totalSum - leftSum - nums[i];
             if (leftSum == rightSum) {
