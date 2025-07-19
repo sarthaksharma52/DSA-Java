5c67dff7f7071f28157846fd034551e70c90058d
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-07-19 13:32:40 +0530

    1 medium level sql query. Q1193

---
+++ SQL/monthlyTransaction.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+select date_format(trans_date, '%Y-%m') as month ,
+country , 
+count(*) as trans_count , 
+count(case when state = "approved" then 1 end) as approved_count , 
+sum(amount) as trans_total_amount ,
+sum(case when state = "approved" then amount else 0 end) as approved_total_amount 
+from 
+transactions
+group by 
+month,country;
+
+
+
+-- using sum in place of count in approved_count
+
+# Write your MySQL query statement below
+select date_format(trans_date, '%Y-%m') as month ,
+country , 
+count(*) as trans_count , 
+sum(case when state = "approved" then 1 else 0 end) as approved_count , 
+sum(amount) as trans_total_amount ,
+sum(case when state = "approved" then amount else 0 end) as approved_total_amount 
+from 
+transactions
+group by 
+month,country;
