480f4d013fbe353fe846e676a2eaab481c8a5773
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-09-07 23:53:54 +0530

    1 medium level backtracking question. Q131

---
+++ BackTracking/palindromePartitioning.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result,new ArrayList<>(),s,0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, List<String> temp , String s, int start){
+        if(start == s.length()){
+            result.add(new ArrayList<>(temp));
+        }
+        for(int i=start;i<s.length();i++){
+            if(isPalindrome(s,start,i)){
+                temp.add(s.substring(start,i+1));
+                backtrack(result,temp,s,i+1);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s,int start, int i){
+        while(start<i){
+            if(s.charAt(start++) != s.charAt(i--)) return false;
+        }
+        return true;
+    }
+}
