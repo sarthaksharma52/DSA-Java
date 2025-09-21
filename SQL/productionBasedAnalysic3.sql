c4027d9b34bddc12f5acc2340db318a87b849e35
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-09-21 23:48:12 +0530

    code update.

--- SQL/productionBasedAnalysic3.sql
+++ SQL/productionBasedAnalysic3.sql
@@ -12,3 +12,22 @@ JOIN (
     GROUP BY product_id
 ) t
 ON s.product_id = t.product_id AND s.year = t.first_year;
+
+
+-- 
+
+SELECT 
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM Sales s
+JOIN (
+    SELECT 
+        product_id, 
+        MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) t
+ON s.product_id = t.product_id AND s.year = t.first_year;
+
