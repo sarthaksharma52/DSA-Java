f23047e616b26d17b91e401c4d0bffc086c6ab39
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-09-23 00:13:27 +0530

    update code.

--- hashmap/firstUniqueCharacter.java
+++ hashmap/firstUniqueCharacter.java
@@ -20,3 +20,22 @@ class Solution {
         return -1; // if no unique character exists
     }
 }
+
+
+// much better approach.
+
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> mp = new LinkedHashMap<>();
+        for(int i=0;i<s.length();i++){
+            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(mp.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
