e1d28ca94bd71c608dc3fd211100411f207fd238
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-09-25 23:02:47 +0530

    1 easy level hashmap question. Q3005

---
+++ hashmap/currentElementWithMaximumFrequency.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        int maxFreq = 0;
+        for (int val : freq.values()) {
+            maxFreq = Math.max(maxFreq, val);
+        }
+
+        int total = 0;
+        for (int val : freq.values()) {
+            if (val == maxFreq) {
+                total += val;
+            }
+        }
+
+        return total;
+    }
+}
