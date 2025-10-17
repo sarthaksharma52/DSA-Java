519086559a4511d8b23172f5944fab8077fb9a48
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-10-17 23:42:16 +0530

    some change

--- PrefixSum/checkIfallTheIntegerAreInRangeCovered.java
+++ PrefixSum/checkIfallTheIntegerAreInRangeCovered.java
@@ -50,3 +50,60 @@ class Solution {
         return true;
     }
 }
+
+
+// 
+
+
+package PrefixSum;
+
+class Solution {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        boolean[] covered = new boolean[51]; 
+
+        for (int[] range : ranges) {
+            int start = range[0];
+            int end = range[1];
+
+            for (int i = start; i <= end; i++) {
+                covered[i] = true;
+            }
+        }
+
+        for (int i = left; i <= right; i++) {
+            if (!covered[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
+
+package PrefixSum;
+
+class Solution {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        boolean[] covered = new boolean[51]; 
+
+        for (int[] range : ranges) {
+            int start = range[0];
+            int end = range[1];
+
+            for (int i = start; i <= end; i++) {
+                covered[i] = true;
+            }
+        }
+
+        for (int i = left; i <= right; i++) {
+            if (!covered[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
