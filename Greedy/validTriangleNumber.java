f3dd8389f323fffe8b220d3fd04001b6d14fd9a6
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-11-05 22:57:49 +0530

    small change in logic

--- Greedy/validTriangleNumber.java
+++ Greedy/validTriangleNumber.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 for(int k=j+1;k<nums.length;k++){
-                    if(nums[i]+nums[j]>nums[k] && nums[i] != 0 && nums[j] != 0 && nums[k] != 0 ){
+                    if(nums[i]+nums[j]>nums[k]){
                         count++;
                     }
                 }
