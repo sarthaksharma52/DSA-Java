7cd89ab16d81863b8550ae58abb6c040076cd25f
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-11-16 23:17:27 +0530

    1 medium level sql question Q550

---
+++ SQL/gamePlayAnalysis4.sql
@@ -0,0 +1,13 @@
+WITH first_login AS (
+  SELECT player_id, MIN(event_date) AS first_date
+  FROM Activity
+  GROUP BY player_id
+)
+SELECT
+  ROUND(
+    SUM(CASE WHEN a2.player_id IS NOT NULL THEN 1 ELSE 0 END) / COUNT(*) , 2
+  ) AS fraction
+FROM first_login f
+LEFT JOIN Activity a2
+  ON a2.player_id = f.player_id
+  AND a2.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY);
