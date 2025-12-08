03c8b24268995ea6bb98ba6c240ee0ff4cf8bce7
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-12-08 22:09:00 +0530

    new updates in code.

--- BinaryTree/leafSimilarTree.java
+++ BinaryTree/leafSimilarTree.java
@@ -45,3 +45,41 @@ class Solution {
         find(root.right,ls);
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        List<Integer> lp = new ArrayList<>();
+        List<Integer> lq = new ArrayList<>();
+        find(p,lp);
+        find(q,lq);
+
+        return lp.equals(lq);
+    }
+
+    private void find(TreeNode root, List<Integer> ls){
+        if(root == null){
+            ls.add(null);
+            return;
+        }
+
+        ls.add(root.val);
+        find(root.left,ls);
+        find(root.right,ls);
+    }
+}
