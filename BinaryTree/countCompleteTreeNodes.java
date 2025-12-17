fae8f532e03fce82e727b87395bf6a50e18ccbde
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-12-17 23:27:47 +0530

    updated code.

--- BinaryTree/countCompleteTreeNodes.java
+++ BinaryTree/countCompleteTreeNodes.java
@@ -15,7 +15,7 @@ package BinaryTree;
  *     }
  * }
  */
-public class Solution {
+public class countCompleteTreeNodes {
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
 
@@ -39,3 +39,40 @@ public class Solution {
     }
 }
 
+
+
+// brute force
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        List<Integer> ls = new ArrayList<>();
+        find(root,ls);
+        return ls.size();
+    }
+
+    private void find(TreeNode root, List<Integer> ls){
+        if(root == null){
+            return;
+        }
+        ls.add(root.val);
+        find(root.left,ls);
+        find(root.right,ls);
+    }
+}
