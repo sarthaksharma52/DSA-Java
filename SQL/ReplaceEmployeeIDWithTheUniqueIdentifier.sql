2ebde8cd62c8f329f6cd8d5c3863b920de3830c7
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-12-19 23:49:59 +0530

    1 easy sql query updated.

--- SQL/ReplaceEmployeeIDWithTheUniqueIdentifier.sql
+++ SQL/ReplaceEmployeeIDWithTheUniqueIdentifier.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
 select EmployeeUNI.unique_id , Employees.name from employees Left join EmployeeUNI on  Employees.id = EmployeeUNI.id;
+
+
+# Write your MySQL query statement below
+select Eu.unique_id as unique_id , E.name as name from Employees E left join EmployeeUNI Eu on E.id = Eu.id
