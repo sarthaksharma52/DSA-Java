7c574fb76b4b8d0a553dc0ac37a604c363e033c5
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-12-24 23:54:02 +0530

    update the sql query

--- SQL/bigCountries.sql
+++ SQL/bigCountries.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
 select name , population , area from World where area >= 3000000 or population >= 25000000;
+
+
+-- update
+
+
+# Write your MySQL query statement below
+select name , population , area from World where area >= 3000000 or population >= 25000000;
