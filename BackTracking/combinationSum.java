a583d5c83ca71fd12dba663453fb115ff8902c84
Sarthak Sharma <130988802+sarthaksharma52@users.noreply.github.com>
2025-12-25 23:59:55 +0530

    Fix formatting in combinationSum method return statement

--- BackTracking/combinationSum.java
+++ BackTracking/combinationSum.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         backtrack(ans,new ArrayList<>(), candidates , target , 0);
-        return ans;
+        return ans; 
     }
 
     private void backtrack(List<List<Integer>> ans,List<Integer> temp,int[] candidates,int target,int start){
