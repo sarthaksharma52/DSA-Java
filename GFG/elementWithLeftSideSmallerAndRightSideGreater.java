10a5c56c1de84831b47d3acf7774c71154e5dded
Sarthak Sharma <130988802+sarthaksharma52@users.noreply.github.com>
2025-12-29 23:14:09 +0530

    Add findElement method to identify specific array element
    Implement logic to find an element with smaller left and greater right.

--- GFG/elementWithLeftSideSmallerAndRightSideGreater.java
+++ GFG/elementWithLeftSideSmallerAndRightSideGreater.java
@@ -20,3 +20,29 @@ class Solution {
         
     }
 }
+
+
+
+
+class Solution {
+    public int findElement(int[] arr) {
+        // code here
+        
+        int n=arr.length;
+        int i=1,j=n-2;
+        
+        while(i<=j){
+            if(arr[i]<arr[i-1]){
+                i++;
+            }
+            else if(arr[j]<arr[j-1]){
+                j--;
+            }
+            else break;
+        }
+        if(i==j) return arr[i];
+       
+        return -1;
+        
+    }
+}
